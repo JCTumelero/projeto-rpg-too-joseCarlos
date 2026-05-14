45777fd028c165d070f4c76009756df3ae43e1d9
JCTumelero <josetumelero.pf001@academico.ifsul.edu.br>
2026-05-14 09:47:29 -0300

    Update teste_trabalho1.py

--- teste_trabalho1.py
+++ teste_trabalho1.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#asiciona o diretório raiz do projeto ao sys.path para permitir a importação dos módulos
+
 from missao_combate import MissaoCombate
 from missao_coleta import MissaoColeta
 from missao_exploracao import MissaoExploracao 
